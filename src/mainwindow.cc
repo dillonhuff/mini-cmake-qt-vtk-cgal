799c645f6ceb14fee6855d58947855c45109116e
Dillon Huff <dillonhuff@gmail.com>
2017-03-19 14:58:42 -0700

    Adding real vtk rendering code

--- src/mainwindow.cc
+++ src/mainwindow.cc
@@ -1,3 +1,12 @@
+#include <vtkSphereSource.h>
+#include <vtkPolyData.h>
+#include <vtkSmartPointer.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -5,6 +14,36 @@ MainWindow::MainWindow(QWidget *parent)
   : QMainWindow(parent)//, ui(new Ui::MainWindow)
 {
   //ui->setupUi(this);
+
+  vtk_window = new QVTKWidget(this, Qt::Widget);
+  setCentralWidget(vtk_window);
+
+  auto m =
+    parse_stl("/Users/dillon/CppWorkspace/gca/test/stl-files/onshape_parts/SmallReverseCameraMount - Part 1.stl", 0.0001);
+
+  vtkSmartPointer<vtkSphereSource> sphereSource = 
+    vtkSmartPointer<vtkSphereSource>::New();
+  sphereSource->SetCenter(0.0, 0.0, 0.0);
+  sphereSource->SetRadius(5.0);
+ 
+  vtkSmartPointer<vtkPolyDataMapper> mapper = 
+    vtkSmartPointer<vtkPolyDataMapper>::New();
+  mapper->SetInputConnection(sphereSource->GetOutputPort());
+ 
+  vtkSmartPointer<vtkActor> actor = 
+    vtkSmartPointer<vtkActor>::New();
+  actor->SetMapper(mapper);
+ 
+  vtkSmartPointer<vtkRenderer> renderer = 
+    vtkSmartPointer<vtkRenderer>::New();
+
+  renderer->SetBackground(1, 1, 1);
+  renderer->AddActor(actor);
+  
+  vtk_window->GetRenderWindow()->AddRenderer(renderer);
+
+  vtk_window->show();
+  
 }
 
 MainWindow::~MainWindow()
